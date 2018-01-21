888b7332921d1de0a6c94dfbf445a843181fa072
Ryan Yogan <ryanyogan@gmail.com>
2018-01-20 16:04:32 -0800

    added in signupMutation

---
+++ src/config/constants.js
@@ -0,0 +1,1 @@
+export const TEMP_APP_SECRET = 'asdsaad22e2jras34';
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,22 @@
+import { hash } from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { TEMP_APP_SECRET } from '../config/constants';
+
 const createTweet = (parent, { body }, ctx, info) =>
   ctx.db.mutation.createTweet({ data: { body } }, info);
 
-export default { createTweet };
+const signup = async (parent, args, ctx) => {
+  const password = await hash(args.password, 10);
+  const [firstName, lastName] = args.fullName.split(' ');
+  const user = await ctx.db.mutation.createUser({
+    data: { ...args, firstName, lastName, password },
+  });
+
+  const token = jwt.sign({ userId: user.id }, TEMP_APP_SECRET);
+  return {
+    token,
+    user,
+  };
+};
+
+export default { createTweet, signup };
